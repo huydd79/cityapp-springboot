6336852ef4ec814a8a0eb2734ff0458e08e0ab5a
Huy Do <huydd79@gmail.com>
2022-12-05 21:17:59 +0700

    Adding first time

---
+++ src/main/java/springboot/huydo/net/CityApp.java
@@ -0,0 +1,13 @@
+package springboot.huydo.net;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CityApp.class, args);
+	}
+
+}
